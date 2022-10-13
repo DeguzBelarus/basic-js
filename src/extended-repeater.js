f89bf2149491fc77e5cdb0e8402338aacccd8fd9
DeguzBelarus <deguz@mail.ru>
2022-10-14 02:31:12 +0300

    solve extended-repeater task (without 1 test)

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,92 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  //   throw new NotImplementedError('Not implemented');
+  let resultString = "";
+
+  let {
+    repeatTimes,
+    separator,
+    addition,
+    additionRepeatTimes,
+    additionSeparator,
+  } = options;
+
+  if (!repeatTimes) {
+    repeatTimes = 1;
+  }
+
+  if (addition && !additionRepeatTimes) {
+    additionRepeatTimes = 1;
+  }
+
+  if (!additionSeparator) {
+    additionSeparator = `|${addition}|`;
+   //  additionRepeatTimes -= 1;
+  }
+
+  if (!separator) {
+    separator = "+";
+  }
+
+  for (let i = 1; i <= repeatTimes; i++) {
+    resultString += str;
+
+    if (i !== repeatTimes) {
+      for (let ii = 1; ii <= additionRepeatTimes; ii++) {
+        if (ii !== additionRepeatTimes) {
+          resultString += addition + additionSeparator;
+        } else {
+          resultString += addition;
+        }
+      }
+
+      resultString += separator;
+    } else {
+      for (let ii = 1; ii <= additionRepeatTimes; ii++) {
+        if (ii !== additionRepeatTimes) {
+          resultString += addition + additionSeparator;
+        } else {
+          resultString += addition;
+        }
+      }
+    }
+  }
+
+  let compare =
+    "REPEATABLE_STRINGADDITION|ADDITION|ADDITION222REPEATABLE_STRINGADDITION|ADDITION|ADDITION";
+
+  if (compare === resultString) {
+    console.log("true");
+  } else {
+    console.log("false");
+    console.log(resultString);
+    console.log(compare);
+    console.log(
+      resultString.length,
+      compare.length,
+      resultString.length - compare.length
+    );
+  }
+
+  return resultString;
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,83 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  throw new NotImplementedError("Not implemented");
+
+  if (!date) return "Unable to determine the time of year!";
+  //   if (arguments[0] === "2022-10-13T13:44:23.413Z")
+  //     throw new Error("Invalid date!");
+  //   console.log(arguments[0]);
+
+  //   let stringDate = date.toString();
+  //   console.log(stringDate);
+  //   stringDate = new Symbol(stringDate);
+  //   console.log(stringDate[toStringTag]);
+
+  //   if (Object.prototype.toString.call(date) === "[object Date]") {
+  //     console.log(date.getTime());
+  //   }
+
+  console.log(typeof date);
+  console.log(Object.getOwnPropertyNames(date));
+  if (Number.isNaN(date.getMonth())) {
+    throw new Error("Invalid date!");
+  }
+
+  const month = date.getMonth();
+
+  console.log(month);
+
+  switch (true) {
+    case month === 0:
+      console.log("winter");
+      return "winter";
+    case month === 1:
+      console.log("winter");
+      return "winter";
+    case month === 2:
+      console.log("spring");
+      return "spring";
+    case month === 3:
+      console.log("spring");
+      return "spring";
+    case month === 4:
+      console.log("spring");
+      return "spring";
+    case month === 5:
+      console.log("summer");
+      return "summer";
+    case month === 6:
+      console.log("summer");
+      return "summer";
+    case month === 7:
+      console.log("summer");
+      return "summer";
+    case month === 8:
+      console.log("autumn");
+      return "autumn";
+    case month === 9:
+      console.log("autumn");
+      return "autumn";
+    case month === 10:
+      console.log("autumn");
+      return "autumn";
+    case month === 11:
+      console.log("winter");
+      return "winter";
+  }
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
